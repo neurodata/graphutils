31ea3776f3ce970af3ba2fa15c85dca3129b992e
loftusa <alexloftus2004@gmail.com>
2019-05-24 17:06:23 -0400

    added docstrings for everything, made pass_to_ranks modify object state rather than return output

--- graphutils/graph_io.py
+++ graphutils/graph_io.py
@@ -19,17 +19,32 @@ class NdmgDirectory:
 
     Attributes
     ----------
-    directory :
-        TODO
-    files : 
-        TODO
-    graphs : 
-        TODO
-    X : 
-        TODO
-    names : 
-        TODO
+    dir : Path
+        Path object to the directory containing edgelists.
+    files : list, sorted
+        List of path objects corresponding to each edgelist.
+    graphs : np.ndarray, shape (n, v, v), 3D
+        Volumetric numpy array, n vxv adjacency matrices corresponding to each edgelist.
+        graphs[0, :, :] corresponds to files[0].
+    X : np.ndarray, shape (n, v*v), 2D
+        numpy array, created by vectorizing each adjacency matrix and stacking.
+    Y : np.ndarray, shape n, 1D
+        subject IDs, sorted set of all subject IDs in `dir`.
+        Y[0] corresponds to files[0].
+
+    Methods
+    -------
+    _X : returns np.ndarray, shape (n, v*v), 2D
+        helper function to create `self.X.`
+        Allows `self.X` to remain a property,
+        but gives functionality for specifying pass-to-ranks on a particular set of graphs.
+    _pass_to_ranks : returns np.ndarray
+        returns an adjacency matrix with pass-to-ranks called on it.
+    save_X_and_Y : returns None
+        Saves `self.X` and `self.Y` into a directory.
     """
+
+    # TODO : tests to make sure that the order of files, graphs, X, and Y all correspond to each other.
 
     def __init__(self, directory):
         if not isinstance(directory, (str, Path)):
@@ -38,6 +53,7 @@ class NdmgDirectory:
             raise TypeError(message)
         else:
             self.dir = Path(directory)
+            self.name = self.dir.name
             self.X = self.X()
 
     def __repr__(self):
@@ -58,26 +74,35 @@ class NdmgDirectory:
         
         Returns
         -------
-        Sorted list of Paths to files in `path`.
+        output : list, sorted
+            Sorted list of Paths to files in `path`.
         """
         # TODO : check if all files are edgelists with the correct stuff in them,
         #        and if they are not,
         #        raise an exception
         correct_suffixes = [".ssv", ".csv"]
-        return sorted(
+        output = sorted(
             [
                 filepath
                 for filepath in self.dir.iterdir()
                 if filepath.suffix in correct_suffixes
             ]
         )
+        return output
 
     @property
     def graphs(self):
         """
-        Returns volumetric numpy array, shape (n, v, v),
+        volumetric numpy array, shape (n, v, v),
         accounting for isolate nodes by unioning the vertices of all component edgelists,
         sorted in the same order as `self.files`.
+
+        Returns
+        -------
+        graphs : np.ndarray, shape (n, v, v), 3D
+            Volumetric numpy array, n vxv adjacency matrices corresponding to each edgelist.
+            graphs[0, :, :] corresponds to files[0].D
+
         """
         # TODO : test to make sure sort order is the same as `self.files`
         list_of_arrays = import_edgelist(self.files)
@@ -85,23 +110,41 @@ class NdmgDirectory:
 
     @property
     def Y(self):
-        # TODO : this will be a numpy array of subject ids,
-        #        sorted the same way as `self.files`.
-        #        use regex to do this.
+        """
+        Get subject IDs
+        
+        Returns
+        -------
+        out : np.ndarray 
+            Array of strings. Each element is a subject ID.
+        """
+        # TODO : test to make sure sort order is the same as `self.files`
         pattern = r"(?<=sub-)(\w*)(?=_ses)"
         names = [re.findall(pattern, str(edgelist))[0] for edgelist in self.files]
         return np.array(names)
 
-    def _X(self, graphs=None, PTR=False):
+    def _X(self, PTR=False):
         """
         this will be a single matrix,
         created by vectorizing each array in `self.graphs`,
         and then appending that array as a row to X.
+
+        Parameters
+        ----------
+        graphs : None or np.ndarray
+            if None, graphs will be `self.graphs`.
+            if not None, 
+        PTR : bool, default False
+            if True, call pass_to_ranks on X.
+
+        Returns
+        -------
+        X : np.ndarray, shape (n, v*v), 2D
+            numpy array, created by vectorizing each adjacency matrix and stacking.
         """
         # TODO : test to make sure order of rows matches order of `self.files`.
         # TODO : test to make sure order within rows matches order within arrays of `self.files`.
-        if graphs is None:
-            graphs = self.graphs
+        graphs = self.graphs.copy()
         if graphs.ndim == 3:
             if PTR:
                 graphs = self._pass_to_ranks(graphs=graphs)
@@ -110,134 +153,57 @@ class NdmgDirectory:
         else:
             raise ValueError("Dimensionality of input must be 3.")
 
-    def _pass_to_ranks(self, method="simple-nonzero", graphs=None):
-        # Just to have this as a utility method
-        if graphs is None:
-            graphs = self.graphs
-        graphs = self.graphs.copy()
-        non_zeros = graphs[graphs != 0]
-        rank = rankdata(non_zeros)
-        normalizer = rank.shape[0]
-        rank = rank / (normalizer + 1)
-        graphs[graphs != 0] = rank
-        return graphs
+    def _pass_to_ranks(self, method="simple-nonzero", on="all"):
+        """
+        pass-to-ranks method, to call on `self.graphs` or `self.X`.
+        When called, modifies one or both of these properties,
+        depending on parameters.
 
-    def save_X_and_Y(self, output_directory):
-        # TODO : this method will save `X` and `Y` as csvs into `output_directory`.
-        np.savetxt(f"{output_directory}_X.csv", self.X, fmt="%f", delimiter=",")
-        np.savetxt(f"{output_directory}_Y.csv", self.Y, fmt="%s")
+        Parameters
+        ----------
+        method : str
+            how to pass to ranks
+        on : str, "all", "X", or "graphs"
+            if all, call pass to ranks on `self.X` and `self.graphs`
+            if X, call pass to ranks on `self.X`
+            if graphs, call pass to ranks on `self.graphs`.
+        """
+        # TODO : make this functionality change state of self.X and self.graphs.
+        def functionality(graphs):
+            non_zeros = graphs[graphs != 0]
+            rank = rankdata(non_zeros)
+            normalizer = rank.shape[0]
+            rank = rank / (normalizer + 1)
+            graphs[graphs != 0] = rank
+            return graphs
 
+        if on == "X":
+            graphs = self.X
+        elif on == "graphs":
+            graphs = self.graphs.copy()
+        else:
+            raise ValueError("`on` must be all, X, or graphs.")
 
-f = NdmgDirectory(
-    "/Users/alex/Dropbox/NeuroData/ndmg-paper/data/graphs/native_graphs_NKI"
-)
-f.X(PTR=True)
-#%%
+        return functionality(graphs)
 
-# ---------- Depracated code below ---------- #
+    def save_X_and_Y(self, output_directory="."):
+        # TODO : test this method
+        """
+        Save `self.X` and `self.Y` into an output directory.
 
+        Parameters
 
-def return_sorted_graph(edgelist: str, n_nodes: int, delimiter=" ", nodetype=int):
-    """From a graph file with n_nodes, return a sorted adjacency matrix.
-
-    Parameters
-    ----------
-    edgelist : str or Path
-        filepath to edgelist file.
-    n_nodes : int
-        number of nodes the numpy array should have.
-
-    Returns
-    -------
-    out : np.ndarray
-        numpy array with shape n_nodes x n_nodes. Adjacency matrix.
-    """
-    # TODO : Should be good. Needs tests.
-    graph = nx.read_weighted_edgelist(edgelist, nodetype=nodetype, delimiter=delimiter)
-    vertices = np.arange(1, n_nodes + 1)
-    out = nx.to_numpy_array(graph, nodelist=vertices, dtype=np.float)
-    return out
-
-
-def path_files_list(path: str):
-    """
-    From a directory containing edgelist files, 
-    return a list of adjacency matrices, 
-    sorted alphabetically.
-    
-    Parameters
-    ----------
-    path : directory of edgelist files.
-    
-    Returns
-    -------
-    Sorted list of paths to files in `path`.
-    """
-    # TODO : try/except to make sure `path` is a directory
-    path = Path(path).absolute()
-    if path.is_dir():
-        return sorted([str(fl) for fl in path.iterdir()])
-    else:
-        raise NotADirectoryError("`path` must be a directory.")
+        Returns
+        -------
+        X : np.ndarray, shape (n, v*v), 2D
+            numpy array, created by vectorizing each adjacency matrix and stacking.
+        """
+        if output_directory == ".":
+            output_directory = os.getcwd()
+        np.savetxt(
+            f"{output_directory}/{self.name}_X.csv", self.X, fmt="%f", delimiter=","
+        )
+        np.savetxt(f"{output_directory}/{self.name}_Y.csv", self.Y, fmt="%s")
 
 
-def get_X(arrays: List, PTR=True):
-    """
-    Ravel every array in arrays, 
-    return single array with rows corresponding to each raveled array.
-
-    Parameters
-    ----------
-    arrays : List
-        list of arrays, each the same size.
-    
-    PTR : bool
-        if True, call pass_to_ranks.
-
-
-    Returns
-    -------
-    X : Concatenated X matrix.
-    """
-    # TODO: check to make sure all of the arrays are the same length
-    # TODO: make sure nodes aren't being sorted differently for each array
-
-    # pass to ranks first
-    if PTR:
-        try:
-            arrays = [pass_to_ranks(array) for array in arrays]
-        except ValueError:
-            # Arrays shouldn't have to be square, and pass_to_ranks only works if square.
-            print("Pass to Ranks not done -- nonsquare matrix")
-            pass
-
-    # stack 'em up
-    return np.stack(list(map(np.ravel, arrays)), axis=0)
-
-
-def get_Y(list_of_ndmg_paths):
-    """
-    From a list of paths to graph files, return a list of the subjects those graphs belong to.
-
-    Parameters
-    ----------
-    list_of_ndmg_paths : list of paths to individual graphs.
-
-    Returns
-    -------
-    Y : 1d array, shape (n_samples), type(str)
-    """
-    # TODO : get target vector from ndmg graphs directory
-    # TODO : turn this into general form.
-    subrgx = re.compile(r"(sub-)([0-9]*)(_)")
-
-    # used to be a list comprehension, but this is more clear
-    subjects_list = []
-    for path in list_of_ndmg_paths:
-        # to make sure there aren't any random files
-        if Path(path).suffix == ".ssv":
-            subject_re = re.search(subrgx, path)
-            subject = subject_re.group(2)
-            subjects_list.append(subject)
-
-    return np.array(subjects_list)
+# TODO : test this
