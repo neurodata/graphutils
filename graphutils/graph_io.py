a2b42f8651fb7526542a38685e32ff5b4e9692e1
loftusa <alexloftus2004@gmail.com>
2019-05-30 21:30:13 -0400

    Allow for arbitrary finding of files from input directory

--- graphutils/graph_io.py
+++ graphutils/graph_io.py
@@ -4,9 +4,11 @@
 from pathlib import Path
 import os
 import re
+import glob
 from typing import List
 from functools import reduce
 from collections import namedtuple
+import warnings
 
 import networkx as nx
 import numpy as np
@@ -21,12 +23,13 @@ class NdmgDirectory:
 
     Attributes
     ----------
-    dir : Path
-        Path object to the directory containing edgelists.
+
+    delimiter : str
+        The delimiter used in edgelists.
+    directory : Path
+        Path object to the directory passed to NdmgDirectory.
     name : str
         Base name of directory.
-    delimiter : str
-        delimiter in edgelist files.
     files : list, sorted
         List of path objects corresponding to each edgelist.
     vertices : np.ndarray
@@ -34,49 +37,32 @@ class NdmgDirectory:
     graphs : np.ndarray, shape (n, v, v), 3D
         Volumetric numpy array, n vxv adjacency matrices corresponding to each edgelist.
         graphs[0, :, :] corresponds to files[0].
-    X : np.ndarray, shape (n, v*v), 2D
-        numpy array, created by vectorizing each adjacency matrix and stacking.
-    Y : np.ndarray, shape n, 1D
+    subjects : np.ndarray, shape n, 1D
         subject IDs, sorted set of all subject IDs in `dir`.
         Y[0] corresponds to files[0].
-
-    Methods
-    -------
-    _pass_to_ranks : returns None
-        change state of object.
-        calls pass to ranks on `self.graphs`, `self.X`, or both.
-    save_X_and_Y : returns None
-        Saves `self.X` and `self.Y` into a directory.
     """
-
-    # TODO : functionality for calculating discriminability
-    # TODO : add delimiter everywhere
 
     def __init__(self, directory, delimiter=" "):
         if not isinstance(directory, (str, Path)):
-            # TODO : {type(directory)} isn't returning output in `message`
             message = f"Directory must be type str or Path. Instead, it is type {type(directory)}."
             raise TypeError(message)
         else:
-            self.dir = Path(directory)
-            self.name = self.dir.name
             self.delimiter = delimiter
-            self.files = self._files()
+            self.directory = Path(directory)
+            self.name = self.directory.name
+            self.files = self._files(directory)
             self.vertices = self._vertices()
             self.graphs = self._graphs()
-            self.X = self._X()
-            self.Y = self._Y()
+            self.subjects = self._subjects()
 
         # works because of falsiness of 0
         if not len(self.files):
             print("Warning : There are no edgelists in this directory.")
 
-        print("initiated")
-
     def __repr__(self):
-        return f"NdmgDirectory obj at {str(self.dir)}"
+        return f"NdmgDirectory obj at {str(self.directory)}"
 
-    def _files(self):
+    def _files(self, directory):
         """
         From a directory containing edgelist files, 
         return a list of edgelist files, 
@@ -96,15 +82,29 @@ class NdmgDirectory:
         # TODO : check if all files are edgelists with the correct stuff in them,
         #        and if they are not,
         #        raise an exception
+        # correct_suffixes = {".ssv", ".csv"}
+        # edgelists = glob.glob(f'{directory}/**/*.ssv', recursive=True)
+        # return sorted(edgelists)  # TODO
+
+        # output = sorted(
+        #     [
+        #         filepath
+        #         for filepath in self.directory.iterdir()
+        #         if filepath.suffix in correct_suffixes
+        #     ]
+        # )
+        # return output
+        # TODO : starting at `roi_connectomes` would make this faster, but more fragile
+        # TODO : test to make sure all filenames exist
+        output = []
         correct_suffixes = {".ssv", ".csv"}
-        output = sorted(
-            [
-                filepath
-                for filepath in self.dir.iterdir()
-                if filepath.suffix in correct_suffixes
-            ]
-        )
-        return output
+        for dirname, _, files in os.walk(directory):
+            for filename in files:
+                if Path(filename).suffix in correct_suffixes:
+                    output.append(Path(dirname) / Path(filename))
+
+        assert all([filename.exists() for filename in output]), "Filenames do not exist."
+        return sorted(output)
 
     @property
     def _nx_graphs(self):
@@ -139,9 +139,11 @@ class NdmgDirectory:
 
         """
         list_of_arrays = import_edgelist(self.files, delimiter=self.delimiter)
-        return np.array(list_of_arrays)
-
-    def _Y(self):
+        if not isinstance(list_of_arrays, list):
+            list_of_arrays = [list_of_arrays]
+        return np.atleast_3d(list_of_arrays)
+    
+    def _subjects(self):
         """
         Get subject IDs
         
@@ -153,6 +155,25 @@ class NdmgDirectory:
         pattern = r"(?<=sub-)(\w*)(?=_ses)"
         names = [re.findall(pattern, str(edgelist))[0] for edgelist in self.files]
         return np.array(names)
+
+class NdmgDiscrim(NdmgDirectory):
+    """
+    Parameters
+    ----------
+    X : np.ndarray, shape (n, v*v), 2D  # TODO
+        numpy array, created by vectorizing each adjacency matrix and stacking.
+
+    Methods
+    -------
+    pass_to_ranks : returns None 
+        change state of object.
+        calls pass to ranks on `self.graphs`, `self.X`, or both.
+    save_X_and_Y : returns None
+        Saves `self.X` and `self.Y` into a directory.
+    """
+    def __init__(self, directory, delimiter=" "):
+        super().__init__(directory, delimiter)
+        self.X = self._X()
 
     def _X(self):
         """
@@ -176,10 +197,13 @@ class NdmgDirectory:
         if self.graphs.ndim == 3:
             n, v1, v2 = np.shape(self.graphs)
             return np.reshape(self.graphs, (n, v1 * v2))
+        elif len(self.files) == 1:
+            warnings.warn("Only one graph in directory.")
+            return self.graphs
         else:
             raise ValueError("Dimensionality of input must be 3.")
 
-    def _pass_to_ranks(self, on="all"):
+    def pass_to_ranks(self, on="all"):
         """
         pass-to-ranks method, to call on `self.graphs` or `self.X`.
         When called, modifies one or both of these properties,
@@ -217,7 +241,7 @@ class NdmgDirectory:
             raise ValueError("`on` must be all, X, or graphs.")
 
     def save_X_and_Y(self, output_directory="cwd"):
-        # TODO : test this method
+        # TODO : update this method
         """
         Save `self.X` and `self.Y` into an output directory.
 
@@ -243,5 +267,14 @@ class NdmgDirectory:
         np.savetxt(Y_name, self.Y, fmt="%s")
 
         name = namedtuple("name", ["X", "Y"])
-        print("updated")
         return name(X_name, Y_name)
+
+    def discriminability(self):
+        # TODO
+        pass
+
+DATAPATH = "/Users/alex/Dropbox/NeuroData/graphutils/tests/test_data"
+
+directory = "/Users/alex/data/output"
+
+d = NdmgDirectory(directory)
