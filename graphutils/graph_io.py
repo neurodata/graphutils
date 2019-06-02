2262bc3820bef5eaa1d586c5cda2f18df7e156ae
loftusa <alexloftus2004@gmail.com>
2019-06-02 16:37:20 -0400

    change how files are retrieved, add functionality for saving into a directory

--- graphutils/graph_io.py
+++ graphutils/graph_io.py
@@ -2,18 +2,15 @@
 """graph_io: input/output utilities for graphs.
 """
 from pathlib import Path
+import shutil
 import os
 import re
 import glob
-from typing import List
-from functools import reduce
-from collections import namedtuple
+from functools import reduce, partial
 import warnings
 
 import networkx as nx
 import numpy as np
-from scipy.stats import rankdata
-from graspy.utils import pass_to_ranks as PTR
 from graspy.utils import import_edgelist
 
 
@@ -80,13 +77,15 @@ class NdmgDirectory:
             Sorted list of Paths to files in `path`.
         """
         output = []
+        correct_suffixes = [".ssv", ".csv"]
+        keywords = ["sub-", "ds_adj"]
         correct_suffixes = {".ssv", ".csv"}
         for dirname, _, files in os.walk(directory):
             for filename in files:
-                if Path(filename).suffix in correct_suffixes:
+                right_suffix = Path(filename).suffix in correct_suffixes
+                right_filename = all(i in filename for i in keywords)
+                if right_suffix and right_filename:
                     output.append(Path(dirname) / Path(filename))
-
-        # TODO : put this in pytest instead
         return sorted(output)
 
     @property
@@ -139,120 +138,19 @@ class NdmgDirectory:
         names = [re.findall(pattern, str(edgelist))[0] for edgelist in self.files]
         return np.array(names)
 
-class NdmgDiscrim(NdmgDirectory):
-    """
-    Parameters
-    ----------
-    X : np.ndarray, shape (n, v*v), 2D  # TODO
-        numpy array, created by vectorizing each adjacency matrix and stacking.
-
-    Methods
-    -------
-    pass_to_ranks : returns None 
-        change state of object.
-        calls pass to ranks on `self.graphs`, `self.X`, or both.
-    save_X_and_Y : returns None
-        Saves `self.X` and `self.Y` into a directory.
-    """
-    def __init__(self, directory, delimiter=" "):
-        super().__init__(directory, delimiter)
-        self.X = self._X()
-
-    def __repr__(self):
-        return f"NdmgDiscrim obj at {str(self.directory)}"
-
-    def _X(self):
+    def to_directory(self, dst=None):
         """
-        this will be a single matrix,
-        created by vectorizing each array in `self.graphs`,
-        and then appending that array as a row to X.
+        Send all `self.files`to `directory`.
 
         Parameters
         ----------
-        graphs : None or np.ndarray
-            if None, graphs will be `self.graphs`.
-            if not None, 
-        PTR : bool, default False
-            if True, call pass_to_ranks on X.
-
-        Returns
-        -------
-        X : np.ndarray, shape (n, v*v), 2D
-            numpy array, created by vectorizing each adjacency matrix and stacking.
+        directory : str or Path
+            directory to send files to.
         """
-        if self.graphs.ndim == 3:
-            n, v1, v2 = np.shape(self.graphs)
-            return np.reshape(self.graphs, (n, v1 * v2))
-        elif len(self.files) == 1:
-            warnings.warn("Only one graph in directory.")
-            return self.graphs
-        else:
-            raise ValueError("Dimensionality of input must be 3.")
-
-    def pass_to_ranks(self, on="all"):
-        """
-        pass-to-ranks method, to call on `self.graphs` or `self.X`.
-        When called, modifies one or both of these properties,
-        depending on parameters.
-
-        Assigns ranks to all non-zero edges, settling ties using 
-        the average. Ranks are then scaled by 
-        :math:`\frac{rank(\text{non-zero edges})}{\text{total non-zero edges} + 1}`.
-
-        Parameters
-        ----------
-        on : str, "all", "X", or "graphs"
-            if all, call pass to ranks on `self.X` and `self.graphs`
-            if X, call pass to ranks on `self.X`
-            if graphs, call pass to ranks on `self.graphs`.
-        """
-
-        def PTR_functionality(graphs):
-            non_zeros = graphs[graphs != 0]
-            rank = rankdata(non_zeros)
-            normalizer = rank.shape[0]
-            rank = rank / (normalizer + 1)
-            graphs[graphs != 0] = rank
-            return graphs
-
-        if on == "X":
-            self.X = PTR_functionality(self.X)
-        elif on == "graphs":
-            self.graphs = PTR_functionality(self.X)
-        elif on == "all":
-            self.X = PTR_functionality(self.X)
-            self.graphs = PTR_functionality(self.graphs)
-        else:
-            raise ValueError("`on` must be all, X, or graphs.")
-
-    def save_X_and_Y(self, output_directory="cwd"):
-        """
-        Save `self.X` and `self.subjects` into an output directory.
-
-        Parameters
-        ----------
-        output_directory : str, default current working directory
-            Directory in which to save the output.
-
-        Returns
-        -------
-        namedtuple with str
-            namedtuple of `name.X, name.Y`. Paths to X and Y.
-        """
-        if output_directory == "cwd":
-            output_directory = Path.cwd()
-        p = Path(output_directory)
+        # TODO : test
+        if dst is None:
+            dst = self.directory
+        p = Path(dst).resolve()
         p.mkdir(parents=True, exist_ok=True)
-
-        X_name = f"{str(p)}/{self.name}_X.csv"
-        Y_name = f"{str(p)}/{self.name}_Y.csv"
-
-        np.savetxt(X_name, self.X, fmt="%f", delimiter=",")
-        np.savetxt(Y_name, self.subjects, fmt="%s")
-
-        name = namedtuple("name", ["X", "Y"])
-        return name(X_name, Y_name)
-
-    def discriminability(self):
-        # TODO
-        pass
+        for filename in self.files:
+            shutil.copy(filename, p)
